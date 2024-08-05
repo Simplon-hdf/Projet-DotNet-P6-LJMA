ff2c29e8a1702740ff14f1bcbbc5fb03fca2436c
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-05 16:09:58 +0200

    feat(Vlog.cs): Create model for Vlog entity

---
+++ Projet-DotNet-P6-LJMA/Models/Vlog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>
+/// Cette classe représente l'entité Vlog de la BDD.
+/// </summary>
+namespace Projet_DotNet_P6_LJMA.Models
+{
+	public class Vlog
+	{
+
+		[Key]
+		[Required]
+		public int id_vlog { get; set; }
+		[Required]
+		[StringLength(50)]
+		public string nom_video { get; set; }
+		[Required]
+		[StringLength(2000)]
+		public string url_video { get; set; }
+		[Required]
+        [StringLength(2000)]
+        public string desc_video { get; set; }
+
+	}
+}
+
