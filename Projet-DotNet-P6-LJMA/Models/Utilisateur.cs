d59544d415e8b757307e2428d1b767229ac1b6a9
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-05 15:08:06 +0200

    feat (Utilisateur) : Ajout des annotation du modèle Utilisateur en BDD

--- Projet-DotNet-P6-LJMA/Models/Utilisateur.cs
+++ Projet-DotNet-P6-LJMA/Models/Utilisateur.cs
@@ -1,12 +1,21 @@
-﻿namespace Projet_DotNet_P6_LJMA.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projet_DotNet_P6_LJMA.Models
 {
     public class Utilisateur
     {
+        [Key]
+        [Required]
         int id_utilisateur { get; set; }
+        [StringLength(35)]
         string? prenom_utilisateur { get; set; }
+        [StringLength(35)]
         string? nom_utilisateur { get; set; }
+        [StringLength(20)]
         string? telephone_utilisateur { get; set; }
+        [StringLength(255)]
         string? mot_de_passe_utilisateur { get; set; }
+        [StringLength(320)]
         string? mail_utilisateur {  get; set; }
     }
 }
