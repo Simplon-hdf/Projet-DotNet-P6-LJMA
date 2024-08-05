cb0310a268185e3c3aa1a9d874ba5caac5a1c43b
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-05 15:57:54 +0200

    feat(Reserver.cs): Create model for Reserver entity

---
+++ Projet-DotNet-P6-LJMA/Models/Reserver.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Cette classe représente l'entité Reserver de la BDD.
+/// </summary>
+namespace Projet_DotNet_P6_LJMA.Models
+{
+	public class Reserver
+	{
+
+		[Key]
+		[Required]
+		public int id_utillisateur { get; set; }
+		public int id_session { get; set; }
+
+	}
+}
