590968c03035c4b598551ae76c33cd1a7373e7d3
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-05 14:44:29 +0200

    feat(Utilisateur.cs) : Création du model Utilisateur

---
+++ Projet-DotNet-P6-LJMA/Models/Utilisateur.cs
@@ -0,0 +1,12 @@
+﻿namespace Projet_DotNet_P6_LJMA.Models
+{
+    public class Utilisateur
+    {
+        int id_utilisateur { get; set; }
+        string? prenom_utilisateur { get; set; }
+        string? nom_utilisateur { get; set; }
+        string? telephone_utilisateur { get; set; }
+        string? mot_de_passe_utilisateur { get; set; }
+        string? mail_utilisateur {  get; set; }
+    }
+}
