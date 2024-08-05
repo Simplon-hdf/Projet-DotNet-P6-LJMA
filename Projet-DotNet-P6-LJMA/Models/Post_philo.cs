5c028b179aee779a1c201ef9e748006204378108
Justin <justindidelot8@outlook.fr>
2024-08-05 14:53:05 +0200

    feat(Post_philo.cs): Create model for Post_philo entity

---
+++ Projet-DotNet-P6-LJMA/Models/Post_philo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Cette classe représente l'entité Post_philo de la BDD.
+/// </summary>
+namespace Projet_DotNet_P6_LJMA.Models
+{
+    public class Post_philo
+    {
+        [Key]
+        [Required]
+        public int id_post {  get; set; }
+
+        public string? content_post { get; set; }
+    }
+}
