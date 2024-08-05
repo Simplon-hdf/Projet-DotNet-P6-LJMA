5fbe6f7679a78cc83f28558879cb03066e955d1c
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-05 12:01:02 +0200

    feat(Theme.cs): Create model for Theme entity

---
+++ Projet-DotNet-P6-LJMA/Models/Theme.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Cette classe représente l'entité Theme de la BDD.
+/// </summary>
+public class Theme
+{
+	[Key]
+	[Required]
+	public int Id_theme { get; set; }
+
+	[StringLength(50)]
+	public string? Nom_theme { get; set; }
+}
