836d7b559f2faf44db86188745afb10cf22d1ef4
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-05 16:24:42 +0200

    style(Vlog.cs): Correction des identations

--- Projet-DotNet-P6-LJMA/Models/Vlog.cs
+++ Projet-DotNet-P6-LJMA/Models/Vlog.cs
@@ -7,7 +7,6 @@ namespace Projet_DotNet_P6_LJMA.Models
 {
 	public class Vlog
 	{
-
 		[Key]
 		[Required]
 		public int id_vlog { get; set; }
@@ -19,8 +18,6 @@ namespace Projet_DotNet_P6_LJMA.Models
 		public string url_video { get; set; }
 		[Required]
 		[StringLength(2000)]
-        public string desc_video { get; set; }
-
+		public string desc_video { get; set; }
 	}
 }
-
