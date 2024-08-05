809a237d15b3e2f672963444d676f995ee68ea6e
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-05 15:24:56 +0200

    feat(Session.cs): Create session model

---
+++ Projet-DotNet-P6-LJMA/Models/Session.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// SummCette classe représente l'entité Session de la BDD.
+/// </summary>
+public class Session
+{
+    [Key]
+    [Required]
+    public int id_session{ get; set; }
+
+    [StringLength(50)]
+    public string? nom_session { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime? date_debut_session { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime? date_fin_session { get; set; }
+}
