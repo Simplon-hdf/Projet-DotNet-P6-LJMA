7dcf4a28f0e325f06b90983c283c8306a805374e
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-06 09:53:22 +0200

    docs(Vlog.cs): Déplacement de la documentation de la classe Vlog

--- Projet-DotNet-P6-LJMA/Models/Vlog.cs
+++ Projet-DotNet-P6-LJMA/Models/Vlog.cs
@@ -1,10 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-/// <summary>
-/// Cette classe représente l'entité Vlog de la BDD.
-/// </summary>
 namespace Projet_DotNet_P6_LJMA.Models
 {
+	/// <summary>
+	/// Cette classe représente l'entité Vlog de la BDD.
+	/// </summary>
 	public class Vlog
 	{
 		[Key]
@@ -12,12 +12,12 @@ namespace Projet_DotNet_P6_LJMA.Models
 		public int id_vlog { get; set; }
 		[Required]
 		[StringLength(50)]
-		public string nom_video { get; set; }
+		public string? nom_video { get; set; }
 		[Required]
 		[StringLength(2000)]
-		public string url_video { get; set; }
+		public string? url_video { get; set; }
 		[Required]
 		[StringLength(2000)]
-		public string desc_video { get; set; }
+		public string? desc_video { get; set; }
 	}
 }
