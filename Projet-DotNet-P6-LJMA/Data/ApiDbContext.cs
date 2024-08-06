f49e90d2c94789a819fbb2470e3adfff267b8a55
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-06 11:36:18 +0200

    feat(ApiDbContext): Creation of DBContext

---
+++ Projet-DotNet-P6-LJMA/Data/ApiDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projet_DotNet_P6_LJMA.Models;
+
+namespace Projet_DotNet_P6_LJMA.Data;
+/// <summary>
+/// Cette classe permet d'intéragir avec la BDD.
+/// </summary>
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    { 
+    }
+
+    public DbSet<Theme> Themes { get; set; }
+    public DbSet<Session> Sessions { get; set; }
+    public DbSet<Reserver> Reservers { get; set; }
+    public DbSet<Post_philo> Post_Philos { get; set; }
+    public DbSet<Randonnee> Randonnees { get; set; }
+    public DbSet<Utilisateur> Utilisateurs { get; set; }
+    public DbSet<Vlog> Vlogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Theme>().ToTable("Theme");
+        modelBuilder.Entity<Session>().ToTable("Session");
+        modelBuilder.Entity<Reserver>().ToTable("Reserver");
+        modelBuilder.Entity<Post_philo>().ToTable("Post_Philos");
+        modelBuilder.Entity<Randonnee>().ToTable("Randonnee");
+        modelBuilder.Entity<Utilisateur>().ToTable("Utilisateur");
+        modelBuilder.Entity<Vlog>().ToTable("Vlog");
+    }
+}
