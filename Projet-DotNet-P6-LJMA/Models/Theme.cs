84653c03a7e53978c52164e401b8e44456dfc3cd
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-06 09:58:55 +0200

    build(Theme.cs): Add namespace

--- Projet-DotNet-P6-LJMA/Models/Theme.cs
+++ Projet-DotNet-P6-LJMA/Models/Theme.cs
@@ -1,14 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-/// <summary>
-/// Cette classe représente l'entité Theme de la BDD.
-/// </summary>
-public class Theme
+namespace Projet_DotNet_P6_LJMA.Models
 {
-	[Key]
-	[Required]
-	public int id_theme { get; set; }
+	/// <summary>
+	/// Cette classe représente l'entité Theme de la BDD.
+	/// </summary>
+	public class Theme
+	{
+		[Key]
+		[Required]
+		public int id_theme { get; set; }
 
-	[StringLength(50)]
-	public string? nom_theme { get; set; }
+		[StringLength(50)]
+		public string? nom_theme { get; set; }
+	}
 }
