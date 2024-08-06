1a55a695a2c2020716379c64f1b08b5628b5c8fa
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-06 10:00:23 +0200

    build(Session.cs): Add namespace

--- Projet-DotNet-P6-LJMA/Models/Session.cs
+++ Projet-DotNet-P6-LJMA/Models/Session.cs
@@ -1,20 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-/// <summary>
-/// SummCette classe représente l'entité Session de la BDD.
-/// </summary>
-public class Session
+namespace Projet_DotNet_P6_LJMA.Models
 {
-    [Key]
-    [Required]
-    public int id_session{ get; set; }
+    /// <summary>
+    /// SummCette classe représente l'entité Session de la BDD.
+    /// </summary>
+    public class Session
+    {
+        [Key]
+        [Required]
+        public int id_session{ get; set; }
 
-    [StringLength(50)]
-    public string? nom_session { get; set; }
+        [StringLength(50)]
+        public string? nom_session { get; set; }
 
-    [DataType(DataType.Date)]
-    public DateTime? date_debut_session { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? date_debut_session { get; set; }
 
-    [DataType(DataType.Date)]
-    public DateTime? date_fin_session { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? date_fin_session { get; set; }
+    }
 }
