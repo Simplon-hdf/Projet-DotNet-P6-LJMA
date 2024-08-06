3fe61bbaafba5fbb7f87ccdd2c3143a3805bcd30
Justin <justindidelot8@outlook.fr>
2024-08-06 10:00:46 +0200

    docs(Post_philo.cs): Change annotation

--- Projet-DotNet-P6-LJMA/Models/Post_philo.cs
+++ Projet-DotNet-P6-LJMA/Models/Post_philo.cs
@@ -1,10 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-/// <summary>
-/// Cette classe représente l'entité Post_philo de la BDD.
-/// </summary>
+
 namespace Projet_DotNet_P6_LJMA.Models
 {
+    /// <summary>
+    /// Cette classe représente l'entité Post_philo de la BDD.
+    /// </summary>
     public class Post_philo
     {
         [Key]
