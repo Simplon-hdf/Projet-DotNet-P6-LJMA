7290040d7954c66bf75915e2287935f1dbf6c29d
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-07 12:54:50 +0200

    feat(SessionRepository.cs): Create repository of Session

---
+++ Projet-DotNet-P6-LJMA/Repositories/SessionRepository.cs
@@ -0,0 +1,55 @@
+﻿using Projet_DotNet_P6_LJMA.Data;
+using Projet_DotNet_P6_LJMA.Models;
+using Projet_DotNet_P6_LJMA.Repositories.Interfaces;
+
+namespace Projet_DotNet_P6_LJMA.Repositories
+
+/// <summary>
+/// Repository de la classe Session.
+/// </summary>
+{
+    public class SessionRepository : ISessionRepository
+    {
+        private readonly ApiDbContext _context;
+
+        public SessionRepository(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async IAsyncEnumerable<Session> GetAllAsync()
+        {
+            await foreach (var session in _context.Sessions)
+            {
+                yield return session;
+            }
+        }
+
+        public async Task<Session?> GetByIdAsync(string id)
+        {
+            return await _context.Sessions.FindAsync(id);
+        }
+
+        public async Task CreateAsync(Session session)
+        {
+            _context.Add(session);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Session session)
+        {
+            _context.Update(session);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session != null)
+            {
+                _context.Sessions.Remove(session);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
