4e9284083ff849287b09102f5fade002d9995fcc
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-07 10:28:02 +0200

    feat: création de la classe UtilisateurRepository

---
+++ Projet-DotNet-P6-LJMA/Repositories/UtilisateurRepository.cs
@@ -0,0 +1,20 @@
+﻿using Projet_DotNet_P6_LJMA.Data;
+
+namespace Projet_DotNet_P6_LJMA.Repositories
+{
+    public class UtilisateurRepository
+    {
+        private readonly ApiDbContext _context;
+
+        /// <summary>
+        /// La classe UtilisateurRepository est notre couche d'accès aux données 
+        /// ( Data access Layer )
+        /// et prend en paramètre le <c>context</c> de la base de donnée ( Data Storage )
+        /// </summary>
+        /// <param name="context"> </param>
+        public UtilisateurRepository(ApiDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
