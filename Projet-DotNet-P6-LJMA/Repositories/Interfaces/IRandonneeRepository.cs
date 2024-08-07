349e41650659762a9db4e84ea24222a07e862e90
Justin <justindidelot8@outlook.fr>
2024-08-07 14:15:01 +0200

    feat(IRandonneeRepository.cs): Creation de l'interface RandonneeRepository + Reajustement des namespaces et using

---
+++ Projet-DotNet-P6-LJMA/Repositories/Interfaces/IRandonneeRepository.cs
@@ -0,0 +1,17 @@
+﻿using Projet_DotNet_P6_LJMA.Models;
+
+namespace Projet_DotNet_P6_LJMA.Repositories.Interfaces
+{
+
+    /// <summary>
+    /// Cette interface représente le repository de la classe Reserver.
+    /// </summary>
+    public interface IRandonneeRepository
+    {
+        IAsyncEnumerable<Randonnee> GetAllAsync();
+        Task<Randonnee?> GetByIdAsync(string id);
+        Task CreateAsync(Randonnee randonnee);
+        Task UpdateAsync(Randonnee randonnee);
+        Task DeleteAsync(string id);
+    }
+}
