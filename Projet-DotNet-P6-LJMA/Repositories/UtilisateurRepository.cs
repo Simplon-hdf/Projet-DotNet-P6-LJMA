d10ea01ff1ec6d03f8d01d3bb0ac956e8682569b
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-07 10:55:24 +0200

    feat: creation de l'interface IUtilisateurRepository

--- Projet-DotNet-P6-LJMA/Repositories/UtilisateurRepository.cs
+++ Projet-DotNet-P6-LJMA/Repositories/UtilisateurRepository.cs
@@ -3,6 +3,16 @@ using Projet_DotNet_P6_LJMA.Models;
 
 namespace Projet_DotNet_P6_LJMA.Repositories
 {
+    public interface IUtilisateurRepository
+    {
+        IAsyncEnumerable<Utilisateur> GetAllAsync();
+        Task<Utilisateur> GetByIdAsync(string id);
+        Task CreateAsync(Utilisateur utilisateur);
+        Task UpdateAsync(Utilisateur utilisateur);
+        Task DeleteAsync(string id);
+        Task<bool> ContactExists(string id);
+    }
+
     public class UtilisateurRepository
     {
         private readonly ApiDbContext _context;
