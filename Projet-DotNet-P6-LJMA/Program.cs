ef6155716ee7a29a9038f68dec88cd18411b6181
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-07 12:08:04 +0200

    fix(Program.cs): Changement d'emplacement l'injection de dépandance

--- Projet-DotNet-P6-LJMA/Program.cs
+++ Projet-DotNet-P6-LJMA/Program.cs
@@ -7,10 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IReserverRepository, ReserverRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IReserverRepository, ReserverRepository>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApiDbContext>(options => 
