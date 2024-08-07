8c6dc84c9188e46833762474c78ed64f13011162
Justin <justindidelot8@outlook.fr>
2024-08-07 15:47:55 +0200

    feat(IPostPhiloRepository.cs): Creation de l'interface de Post_philo

---
+++ Projet-DotNet-P6-LJMA/Repositories/Interfaces/IPostPhiloRepository.cs
@@ -0,0 +1,13 @@
+﻿using Projet_DotNet_P6_LJMA.Models;
+
+namespace Projet_DotNet_P6_LJMA.Repositories.Interfaces
+{
+    public interface IPostPhiloRepository
+    {
+        IAsyncEnumerable<Post_philo> GetAllAsync();
+        Task<Post_philo?> GetByIdAsync(string id);
+        Task CreateAsync(Post_philo post_philo);
+        Task UpdateAsync(Post_philo post_philo);
+        Task DeleteAsync(string id);
+    }
+}
