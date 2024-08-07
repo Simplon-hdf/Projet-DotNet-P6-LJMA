c6cb5cf0a18225a793730603a3e826fce1cb5b2f
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-07 11:27:27 +0200

    feat, fix: implémentation et documentations des méthodes GetByIdAsync, GetAllAsync, DeleteAsync, UpdateAsync, UtilisateurExist de la classe UtilisateurRepository + changement tu type de l'identifiant de int en Guid

--- Projet-DotNet-P6-LJMA/Repositories/UtilisateurRepository.cs
+++ Projet-DotNet-P6-LJMA/Repositories/UtilisateurRepository.cs
@@ -1,4 +1,5 @@
-﻿using Projet_DotNet_P6_LJMA.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Projet_DotNet_P6_LJMA.Data;
 using Projet_DotNet_P6_LJMA.Models;
 
 namespace Projet_DotNet_P6_LJMA.Repositories
@@ -6,11 +7,11 @@ namespace Projet_DotNet_P6_LJMA.Repositories
     public interface IUtilisateurRepository
     {
         IAsyncEnumerable<Utilisateur> GetAllAsync();
-        Task<Utilisateur> GetByIdAsync(string id);
+        Task<Utilisateur> GetByIdAsync(Guid id);
         Task CreateAsync(Utilisateur utilisateur);
         Task UpdateAsync(Utilisateur utilisateur);
-        Task DeleteAsync(string id);
-        Task<bool> UtilisateurExist(string id);
+        Task DeleteAsync(Guid id);
+        Task<bool> UtilisateurExist(Guid id);
     }
 
     public class UtilisateurRepository : IUtilisateurRepository
@@ -28,23 +29,8 @@ namespace Projet_DotNet_P6_LJMA.Repositories
             _context = context;
         }
 
-        public async Task<bool> UtilisateurExist(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task CreateAsync(Utilisateur utilisateur)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteAsync(string id)
-        {
-            throw new NotImplementedException();
-        }
-
         /// <summary>
-        /// Récupere les utilisateurs en base de donnée
+        /// Récupère les utilisateurs en base de donnée
         /// </summary>
         /// <returns>
         /// Renvoie une collection d'utilisateurs en asynchrone de type IAsyncEnumerable<Utilisateur>
@@ -57,14 +43,56 @@ namespace Projet_DotNet_P6_LJMA.Repositories
             }
         }
 
-        public Task<Utilisateur> GetByIdAsync(string id)
+        /// <summary>
+        /// Récupère un utilisateur par son identifiant <c>id</c>
+        /// </summary>
+        /// <param name="id">l'identifiant de l'utilisateur</param>
+        /// <returns></returns>
+        public async Task<Utilisateur> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await _context.Utilisateurs.FindAsync(id);
         }
 
-        public Task UpdateAsync(Utilisateur utilisateur)
+        /// <summary>
+        /// Rècupère un booléen si un utilisateur existe dans la base de données
+        /// </summary>
+        /// <param name="id">identifiant de l'utilisateur</param>
+        /// <returns>
+        /// Renvoie True si l'utilisateur dans la base de données et False sinon
+        /// </returns>
+        public async Task<bool> UtilisateurExist(Guid id)
         {
-            throw new NotImplementedException();
+            return await _context.Utilisateurs.AnyAsync(utilisateur => utilisateur.id_utilisateur == id);
+        }
+
+        /// <summary>
+        /// Créer un utilisateur à partir de son identifiant
+        /// </summary>
+        /// <param name="utilisateur"></param>
+        public async Task CreateAsync(Utilisateur utilisateur)
+        {
+            _context.Add(utilisateur);
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Supprime un utilisateur à partir de son identifiant
+        /// </summary>
+        /// <param name="id">identifiant de l'utilisateur</param>
+        public async Task DeleteAsync(Guid id)
+        {
+            _context.Remove(id);
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Mets à jour un utilisateur à partir de <c>utilisateurToUpdate</c>
+        /// </summary>
+        /// <param name="utilisateurToUpdate">le nouveau utilisateur à mettre à jour</param>
+        public async Task UpdateAsync(Utilisateur utilisateurToUpdate)
+        {
+            _context.Utilisateurs.Update(utilisateurToUpdate);
+            await _context.SaveChangesAsync();
         }
     }
 }
