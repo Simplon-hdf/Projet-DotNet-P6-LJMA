b71dd3b2a72dec8051254c6a8cc97abd5573ebb2
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-07 11:29:01 +0200

    feat(ReserverRepository.cs): Ajout du repository Reserver

---
+++ Projet-DotNet-P6-LJMA/Repositories/ReserverRepository.cs
@@ -0,0 +1,52 @@
+﻿using Projet_DotNet_P6_LJMA.Data;
+using Projet_DotNet_P6_LJMA.Models;
+
+namespace Projet_DotNet_P6_LJMA;
+
+/// <summary>
+/// Repository de la classe Reserver.
+/// </summary>
+public class ReserverRepository : IReserverRepository
+{
+    private readonly ApiDbContext _context;
+
+    public ReserverRepository(ApiDbContext context)
+    {
+        _context = context;
+    }
+
+    public async IAsyncEnumerable<Reserver> GetAllAsync()
+    {
+        await foreach (var reserver in _context.Reservers)
+        {
+            yield return reserver;
+        }
+    }
+
+    public async Task<Reserver?> GetByIdAsync(string id)
+    {
+        return await _context.Reservers.FindAsync(id);
+    }
+
+    public async Task CreateAsync(Reserver reserver)
+    {
+        _context.Add(reserver);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Reserver reserver)
+    {
+        _context.Update(reserver);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        var reserver = await _context.Reservers.FindAsync(id);
+        if (reserver != null)
+        {
+            _context.Reservers.Remove(reserver);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
