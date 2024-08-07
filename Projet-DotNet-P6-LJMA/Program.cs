a79d9a22780542ee561104f0b9cd46c6568835b8
Justin <justindidelot8@outlook.fr>
2024-08-07 15:59:42 +0200

    feat(Program.cs): Injection de dependance a couplage faible

--- Projet-DotNet-P6-LJMA/Program.cs
+++ Projet-DotNet-P6-LJMA/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddScoped<IReserverRepository, ReserverRepository>();
 builder.Services.AddScoped<IVlogRepository, VlogRepository>();
 builder.Services.AddScoped<IRandonneeRepository, RandonneeRepository>();
 builder.Services.AddScoped<IThemeRepository, ThemeRepository>();
+builder.Services.AddScoped<IPostPhiloRepository, PostPhiloRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
