0fca64c0838493c3e956b396c0943376a65f2082
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-08 12:39:36 +0200

    feat: Création de Mapping en utilisant le nugget "Riok.Mapperly"

---
+++ Projet-DotNet-P6-LJMA/Mapping/Mapper.cs
@@ -0,0 +1,13 @@
+﻿using Projet_DotNet_P6_LJMA.Models;
+using Projet_DotNet_P6_LJMA.ModelsDTO;
+using Riok.Mapperly.Abstractions;
+
+namespace Projet_DotNet_P6_LJMA.Mapping
+{
+    [Mapper(EnumMappingStrategy = EnumMappingStrategy.ByName)]
+    public static partial class Mapper
+    {
+        public static partial UtilisateurDto MapToDto(this Utilisateur utilisateur);
+        public static partial Utilisateur Map(this UtilisateurDto utilisateur);
+    }
+}
