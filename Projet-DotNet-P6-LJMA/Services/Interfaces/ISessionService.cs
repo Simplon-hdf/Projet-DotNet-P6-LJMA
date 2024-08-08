40a673daa219cadc45252f88edf7fea748f8737b
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-08 16:41:55 +0200

    feat(ISessionService.cs): Creation of ISessionService.cs

---
+++ Projet-DotNet-P6-LJMA/Services/Interfaces/ISessionService.cs
@@ -0,0 +1,13 @@
+﻿using Projet_DotNet_P6_LJMA.ModelsDTO;
+
+namespace Projet_DotNet_P6_LJMA.Services.Interfaces
+{
+    public interface ISessionService
+    {
+        IAsyncEnumerable<SessionDto> GetAllAsync();
+        Task<SessionDto> GetByIdAsync(Guid id);
+        Task CreateAsync(SessionDto session);
+        Task UpdateAsync(SessionDto session);
+        Task DeleteAsync(Guid id);
+    }
+}
