936457d2fe0ea4f4948d4cb3227e6a27b1435e6c
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-08 15:29:29 +0200

    feat(Mapper.cs): Creation du mapping pour Reserver

--- Projet-DotNet-P6-LJMA/Mapping/Mapper.cs
+++ Projet-DotNet-P6-LJMA/Mapping/Mapper.cs
@@ -9,5 +9,9 @@ namespace Projet_DotNet_P6_LJMA.Mapping
     {
         public static partial UtilisateurDto MapToDto(this Utilisateur utilisateur);
         public static partial Utilisateur Map(this UtilisateurDto utilisateur);
+
+        public static partial ReserverDto MapToDto(this Reserver reserver);
+
+        public static partial Reserver Map(this ReserverDto reserver);
     }
 }
