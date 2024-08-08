3a5054a7b0c66d2de9c8f5c7758f4827e04626e6
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-08 15:38:22 +0200

    feat(IReserverService.cs): creation de l'interface de ReserverService

--- Projet-DotNet-P6-LJMA/Services/Interfaces/IReserverService.cs
+++ Projet-DotNet-P6-LJMA/Services/Interfaces/IReserverService.cs
@@ -1,6 +1,12 @@
-﻿namespace Projet_DotNet_P6_LJMA;
+﻿using Projet_DotNet_P6_LJMA.ModelsDTO;
 
-public class IReserverService
+namespace Projet_DotNet_P6_LJMA.Services.Interfaces;
+
+public interface IReserverService
 {
-
+    IAsyncEnumerable<ReserverDto> GetAllAsync();
+    Task<ReserverDto> GetByIdAsync(Guid id);
+    Task CreateAsync(ReserverCreatedDto reserver);
+    Task UpdateAsync(ReserverDto reserver);
+    Task DeleteAsync(Guid id);
 }
