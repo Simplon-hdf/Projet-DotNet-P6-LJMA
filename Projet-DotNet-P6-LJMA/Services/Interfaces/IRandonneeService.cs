c6a91de41f6076f707ea14f2bdc5904c3a83899c
Justin <justindidelot8@outlook.fr>
2024-08-09 10:01:29 +0200

    feat(IRandonneeService.cs): Creation de l'interface de RandonneeService

---
+++ Projet-DotNet-P6-LJMA/Services/Interfaces/IRandonneeService.cs
@@ -0,0 +1,13 @@
+﻿using Projet_DotNet_P6_LJMA.ModelsDTO;
+
+namespace Projet_DotNet_P6_LJMA.Services.Interfaces
+{
+    public interface IRandonneeService
+    {
+        IAsyncEnumerable<RandonneeDto> GetAllAsync();
+        Task<RandonneeDto> GetByIdAsync(Guid id);
+        Task CreateAsync(RandonneeDto randonnee);
+        Task UpdateAsync(RandonneeDto randonnee);
+        Task DeleteAsync(Guid id);
+    }
+}
