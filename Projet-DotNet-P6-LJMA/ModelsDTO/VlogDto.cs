bf8c9e942b85f2868dbca6a59d46caefa958080d
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-09 10:11:08 +0200

    chore: implémentation des model avec les modeldto

--- Projet-DotNet-P6-LJMA/ModelsDTO/VlogDto.cs
+++ Projet-DotNet-P6-LJMA/ModelsDTO/VlogDto.cs
@@ -1,8 +1,9 @@
-using System;
-
 namespace Projet_DotNet_P6_LJMA.ModelsDTO;
 
 public class VlogDto
 {
-
+    public Guid vlogId { get; set; }
+    public string? nomVideo { get; set; }
+    public string? ulrVideo { get; set; }
+    public string? descVideo { get; set; }
 }
