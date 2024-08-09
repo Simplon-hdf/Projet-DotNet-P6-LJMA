511e866b623a30cbcfa2918f62285fc4180a4702
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-09 10:06:20 +0200

    feat: Création de la structure de la couche service Vlog

---
+++ Projet-DotNet-P6-LJMA/Services/Interfaces/IVlogService.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Projet_DotNet_P6_LJMA.Services.Interfaces;
+
+public class IVlogService
+{
+
+}
---
+++ Projet-DotNet-P6-LJMA/Services/VlogService.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Projet_DotNet_P6_LJMA.Services;
+
+public class VlogService
+{
+
+}
