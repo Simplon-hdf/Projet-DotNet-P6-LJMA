4cdac062198e1964151b617e210d28f2ad261c1e
Justin <justindidelot8@outlook.fr>
2024-08-09 12:30:33 +0200

    feat(IPostService.cs): Creation de l'interface de PostService

---
+++ Projet-DotNet-P6-LJMA/Services/Interfaces/IPostService.cs
@@ -0,0 +1,13 @@
+﻿using Projet_DotNet_P6_LJMA.ModelsDTO;
+
+namespace Projet_DotNet_P6_LJMA.Services.Interfaces
+{
+    public interface IPostService
+    {
+        IAsyncEnumerable<PostDto> GetAllAsync();
+        Task<PostDto> GetByIdAsync(Guid id);
+        Task CreateAsync(PostDto post);
+        Task UpdateAsync(PostDto post);
+        Task DeleteAsync(Guid id);
+    }
+}
