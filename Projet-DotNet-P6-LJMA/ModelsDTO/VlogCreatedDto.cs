1fe48e748e40aa9e757330a18f4baebf8cc98280
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-09 16:28:16 +0200

    feat(VlogCreatedDto.cs): Ajout des parametre de Vlog

--- Projet-DotNet-P6-LJMA/ModelsDTO/VlogCreatedDto.cs
+++ Projet-DotNet-P6-LJMA/ModelsDTO/VlogCreatedDto.cs
@@ -2,7 +2,9 @@ using System;
 
 namespace Projet_DotNet_P6_LJMA.ModelsDTO;
 
-public class VlogCreatedDto : VlogDto
+public class VlogCreatedDto
 {
-
+    public string? nomVideo { get; set; }
+    public string? urlVideo { get; set; }
+    public string? descVideo { get; set; }
 }
