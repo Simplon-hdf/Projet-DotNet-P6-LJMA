eb336cc272f62b9409ef4809952fefa59fbca311
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-09 10:21:30 +0200

    feat(VlogsController.cs): Changement du controller

--- Projet-DotNet-P6-LJMA/Services/Interfaces/IVlogService.cs
+++ Projet-DotNet-P6-LJMA/Services/Interfaces/IVlogService.cs
@@ -1,8 +1,12 @@
-using System;
+using Projet_DotNet_P6_LJMA.ModelsDTO;
 
 namespace Projet_DotNet_P6_LJMA.Services.Interfaces;
 
-public class IVlogService
+public interface IVlogService
 {
-
+    IAsyncEnumerable<VlogDto> GetAllAsync();
+    Task<VlogDto> GetByIdAsync(Guid id);
+    Task CreateAsync(VlogCreatedDto vlog);
+    Task UpdateAsync(VlogDto vlog);
+    Task DeleteAsync(Guid id);
 }
