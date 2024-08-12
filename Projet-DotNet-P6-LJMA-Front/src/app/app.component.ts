232a841baedd670ca797cc75b5c1fc779bd7e2a8
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-12 14:24:19 +0200

    feat(app.component.ts): Injection RouterModule package

--- Projet-DotNet-P6-LJMA-Front/src/app/app.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from "@angular/router";
 import {HomePageComponent} from "./Components/home-page/home-page.component";
 import {HeaderComponent} from "./Components/header/header.component";
+import {AboutPageComponent} from "./Components/about-page/about-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent, HeaderComponent],
+  imports: [RouterOutlet, RouterModule, HomePageComponent, HeaderComponent, AboutPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
