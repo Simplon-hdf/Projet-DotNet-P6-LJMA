81b6ecae23a67ee8ff1cebd0e859dddbaf110874
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-12 14:24:46 +0200

    feat(about-page): Initialisation About component

---
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/about-page/about-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.css'
+})
+export class AboutPageComponent {
+
+}
