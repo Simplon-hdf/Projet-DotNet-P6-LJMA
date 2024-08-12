675ff44414717d204b602aa1d850750dc7b611f8
Justin <justindidelot8@outlook.fr>
2024-08-12 09:32:48 +0200

    fix(PostDto.cs): Changement des noms des propriétés

--- Projet-DotNet-P6-LJMA/ModelsDTO/PostDto.cs
+++ Projet-DotNet-P6-LJMA/ModelsDTO/PostDto.cs
@@ -6,10 +6,10 @@ namespace Projet_DotNet_P6_LJMA.ModelsDTO
     public class PostDto
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity), Required]
-        public Guid id_post { get; set; }
+        public Guid Id { get; set; }
         [MaxLength(2000), Required]
-        public string? content_post { get; set; }
+        public string? Content { get; set; }
         [MaxLength(2000)]
-        public string img_post { get; set; }
+        public string Image { get; set; }
     }
 }
