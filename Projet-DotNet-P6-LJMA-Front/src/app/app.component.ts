f0c24035479168ad2715606c074a8d04e2206e58
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-12 14:10:10 +0200

    feat(app.component): Injection script

--- Projet-DotNet-P6-LJMA-Front/src/app/app.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomePageComponent} from "./Components/home-page/home-page.component";
+import {HeaderComponent} from "./Components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent],
+  imports: [RouterOutlet, HomePageComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
