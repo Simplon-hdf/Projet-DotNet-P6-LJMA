db8c1b7014c38c48d99d99646039accf49618acb
Justin <justindidelot8@outlook.fr>
2024-08-12 09:22:12 +0200

    fix(PostDto.cs): Correction des annotations du model PostDto

--- Projet-DotNet-P6-LJMA/ModelsDTO/PostDto.cs
+++ Projet-DotNet-P6-LJMA/ModelsDTO/PostDto.cs
@@ -5,7 +5,7 @@ namespace Projet_DotNet_P6_LJMA.ModelsDTO
 {
     public class PostDto
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity), Required, MaxLength(36)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity), Required]
         public Guid id_post { get; set; }
         [MaxLength(2000), Required]
         public string? content_post { get; set; }
