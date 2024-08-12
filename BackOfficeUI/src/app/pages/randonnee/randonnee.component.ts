620dffd4fc2f8d7a6822a89df5bb23f6e1118c4a
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-12 11:35:59 +0200

    feat(BackOfficeUI/): Creation of BackOffice

---
+++ BackOfficeUI/src/app/pages/randonnee/randonnee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randonnee',
+  standalone: true,
+  imports: [],
+  templateUrl: './randonnee.component.html',
+  styleUrl: './randonnee.component.css'
+})
+export class RandonneeComponent {
+
+}
---
+++ BackOfficeUI/src/app/pages/utilisateur/utilisateur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-utilisateur',
+  standalone: true,
+  imports: [],
+  templateUrl: './utilisateur.component.html',
+  styleUrl: './utilisateur.component.css'
+})
+export class UtilisateurComponent {
+
+}
