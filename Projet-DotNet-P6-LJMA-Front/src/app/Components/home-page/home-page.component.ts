e816a57fef0909f931517b4ee32c01b80c08d611
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-12 14:09:03 +0200

    feat(home-page-component): Mise en forme de la home page

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/home-page/home-page.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/home-page/home-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
