26db4e7f0e5f74576d1d9eeebfc5d66bae811f08
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-14 09:36:18 +0200

    feat(header): Création du component header + mise en forme

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/header/header.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
