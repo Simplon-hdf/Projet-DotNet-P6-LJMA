c0f29a1cf8e3ee02e3277b20cc2d3f404ae8b966
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-14 09:35:22 +0200

    feat(app.config.ts): injection des routes

--- Projet-DotNet-P6-LJMA-Front/src/app/app.config.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/app.config.ts
@@ -1,9 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes)]
 };
