9ab3e7b51aa82b7d5701703a578d96662fa87ff2
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-14 11:27:26 +0200

    feat(register.component): Ajout des contrôle de saisie

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/register/register.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/register/register.component.ts
@@ -1,17 +1,60 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterModule} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import {RouterLink, Router} from "@angular/router";
+import {FormsModule, MaxLengthValidator} from "@angular/forms";
 import {LoginComponent} from "../login/login.component";
+import { UserRegister } from "../../Models/reg.model";
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
     RouterLink,
+    FormsModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  user:UserRegister = {};
 
-  protected readonly LoginComponent = LoginComponent;
+  constructor(private router: Router) {}
+
+  onSubmit(): void {
+    if (this.isValidMail() && this.isValidPassword() && this.isValidNumber()) {
+      console.log("User : " + this.user.email); // Remplacé alert par console.log pour le débogage
+
+      // Ici, vous devriez normalement faire un appel API pour l'authentification
+      // Pour l'instant, nous allons simplement simuler une connexion réussie
+
+      // Redirection
+      this.router.navigate(["/home"]);
+    } else {
+      // Ajout d'un message d'erreur si la validation échoue
+      console.error("Login ou mot de passe invalide");
+      // Vous pouvez afficher un message à l'utilisateur ici
+    }
+  }
+
+  isValidMail(): boolean{
+    const valideurMail : RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    return valideurMail.test(this.user.email!);
+  }
+
+  isValidNumber(): boolean {
+    if (!this.user.tel) {
+      return false; // Le numéro est vide ou non défini
+    }
+
+    // Expression régulière pour les numéros de téléphone français
+    const phonePattern = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/;
+
+    return phonePattern.test(this.user.tel);
+  }
+
+  isValidPassword(): boolean{
+    const valideurMotDePasse  : RegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[\w!@#$%^&*(),.?":{}|<>]{12,}$/gm;
+    return valideurMotDePasse.test(this.user.motDePasse!);
+  }
+
+  ngOnInit(): void {}
 }
