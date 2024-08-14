a040314f36de185bcba42c4eb9891b8e560d5f1d
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-14 09:37:02 +0200

    other

--- Projet-DotNet-P6-LJMA-Front/src/main.ts
+++ Projet-DotNet-P6-LJMA-Front/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+})
+  .catch(err => console.error(err));
