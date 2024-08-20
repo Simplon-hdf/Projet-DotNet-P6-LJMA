26160f18a3c5c7b18d69c36a1339b93282a1ecba
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-20 15:14:13 +0200

    feat(login): Création et mise en forme de la page login + ajour de la fonction

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/login/login.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/login/login.component.ts
@@ -1,47 +1,45 @@
-import {Component, OnInit} from '@angular/core';
-import {RouterLink, Router} from "@angular/router";
-import {FormsModule} from "@angular/forms";
-import {UserCredentials} from "../../Models/auth.model";
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from "@angular/router";
+import { FormsModule, NgForm } from "@angular/forms";
+import { FormLoginService } from "../../Services/form-login.service";
+import { UserCredentials } from "../../Models/auth.model";
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     RouterLink,
-    FormsModule
+    FormsModule,
+    RouterOutlet,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent implements OnInit {
-  user:UserCredentials = {};
-  constructor(private router: Router) {}
+  user: UserCredentials = {};
+  constructor(public service: FormLoginService, public router : Router) {}
 
-  onSubmit(): void {
-    if (this.isValidLogin() && this.isValidPassword()) {
-      console.log("User : " + this.user.login); // Remplacé alert par console.log pour le débogage
-
-      // Ici, vous devriez normalement faire un appel API pour l'authentification
-      // Pour l'instant, nous allons simplement simuler une connexion réussie
-
-      // Redirection
-      this.router.navigate(["/home"]);
-    } else {
-      // Ajout d'un message d'erreur si la validation échoue
-      console.error("Login ou mot de passe invalide");
-      // Vous pouvez afficher un message à l'utilisateur ici
-    }
+  onSubmit(form: NgForm) {
+    this.service.getLogin(this.user.email!, this.user.motDePasse!)
+      .subscribe({
+        next: (res: any) => {
+          console.log(res);
+          // Gérer la réponse de connexion réussie ici
+          this.router.navigate(['/home']);
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err);
+          // Gérer les erreurs de connexion ici
+        }
+      });
   }
 
-  isValidLogin(): boolean{
-    const valideurUtilisateur : RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    return valideurUtilisateur.test(this.user.login!);
+  isValidLogin(): boolean {
+    const valideurUtilisateur: RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    return valideurUtilisateur.test(this.user.email!);
   }
 
-  isValidPassword(): boolean{
-    const valideurMotDePasse  : RegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[\w!@#$%^&*(),.?":{}|<>]{12,}$/gm;
-    return valideurMotDePasse.test(this.user.motDePasse!);
-  }
-
-  ngOnInit(): void {}
+  ngOnInit() {}
 }
