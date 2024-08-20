448e2d0e613668eafa15ef756a8b4edc50775a81
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-20 10:18:18 +0200

    feat(): Template for General page

---
+++ BackOfficeUI/src/app/pages/general/general.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general',
+  standalone: true,
+  imports: [],
+  templateUrl: './general.component.html',
+  styleUrl: './general.component.css'
+})
+export class GeneralComponent {
+  randonnees = [
+    {
+      id: 1,
+      nom: 'Ardenne',
+      participant: 7,
+      nuit: 3,
+      lieu: 'Foret',
+      prix: 134
+    },
+    {
+      id: 2,
+      nom: 'Alpes',
+      participant: 7,
+      nuit: 3,
+      lieu: 'Montagne',
+      prix: 230
+    },
+    {
+      id: 3,
+      nom: 'Pyrenees',
+      participant: 7,
+      nuit: 3,
+      lieu: 'Montagne',
+      prix: 12
+    },
+  ];
+
+  utilisateurs = [
+    {
+      id: 1,
+      nom: 'Jean',
+      prenom: 'Marie',
+      tel: '0679393927',
+      mail: 'mail@exemple.com',
+      role: 'Utilisateur'
+    },
+    {
+      id: 2,
+      nom: 'Claire',
+      prenom: 'Dupont',
+      tel: '0673342927',
+      mail: 'mail@exemple.com',
+      role: 'Utilisateur'
+    },
+    {
+      id: 3,
+      nom: 'Damien',
+      prenom: 'Craine',
+      tel: '0638382728',
+      mail: 'mail@exemple.com',
+      role: 'Admin'
+    },
+  ];
+}
