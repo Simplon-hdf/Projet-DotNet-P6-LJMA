a3eb44c779af29e06271dd990d36bc00efd725ab
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-20 10:47:41 +0200

    feat(register): Changement de formulaire plus mise en place et mise en forme provisoire

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/register/register.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/register/register.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLink, Router } from "@angular/router";
-import { FormsModule } from "@angular/forms";
-import { UserRegister } from "../../Models/reg.model";
+import {Component, Injectable, OnInit} from '@angular/core';
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { FormsModule, NgForm } from "@angular/forms";
+import { FormRegisterService } from "../../Services/form-register.service";
 
 @Component({
   selector: 'app-register',
@@ -9,50 +9,23 @@ import { UserRegister } from "../../Models/reg.model";
   imports: [
     RouterLink,
     FormsModule,
+    RouterOutlet,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit {
-  user:UserRegister = {};
+  constructor(public service : FormRegisterService ) {}
 
-  constructor(private router: Router) {}
-
-  onSubmit(): void {
-    if (this.isValidMail() && this.isValidPassword() && this.isValidNumber()) {
-      console.log("User : " + this.user.email);
-
-      // Ici, vous devriez normalement faire un appel API pour l'authentification
-      // Pour l'instant, nous allons simplement simuler une connexion réussie
-
-      // Redirection
-      this.router.navigate(["/home"]);
-    } else {
-      // Ajout d'un message d'erreur si la validation échoue
-      console.error("Login ou mot de passe invalide");
-      // Vous pouvez afficher un message à l'utilisateur ici
-    }
+  onSubmit(form: NgForm){
+    this.service.postEnregistrer()
+      .subscribe({
+        next:res => {
+          console.log(res)
+        },
+        error:err=>{console.log(err)}
+      })
   }
 
-  isValidMail(): boolean{
-    const valideurMail : RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    return valideurMail.test(this.user.email!);
-  }
-
-  isValidNumber(): boolean {
-    if (!this.user.tel) {
-      return false; // Le numéro est vide ou non défini
-    }
-
-    // Expression régulière pour les numéros de téléphone français
-    const phonePattern : RegExp = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/;
-    return phonePattern.test(this.user.tel);
-  }
-
-  isValidPassword(): boolean{
-    const valideurMotDePasse  : RegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[\w!@#$%^&*(),.?":{}|<>]{12,}$/gm;
-    return valideurMotDePasse.test(this.user.motDePasse!);
-  }
-
-  ngOnInit(): void {}
+  ngOnInit() {}
 }
