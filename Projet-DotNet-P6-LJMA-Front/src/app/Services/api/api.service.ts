eb4ee93b2ec47ee9cefb201be504a158fd47384e
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-21 15:06:00 +0200

    fix(api): fix des bugs de l'api

--- Projet-DotNet-P6-LJMA-Front/src/app/Services/api/api.service.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Services/api/api.service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpEvent} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {environment} from "../../Environments/environment";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  get<T>(URL : string): Observable<T> {
-    return this.http.get<T>(environment.apiBaseUrl + URL);
+  get<T>(url: string): Observable<T> {
+    return this.http.get<T>(url).pipe(
+      catchError(this.handleError)
+    );
   }
 
-  post<T>(URL : string, body:any): Observable<HttpEvent<T>> {
-    return this.http.get<T>(environment.apiBaseUrl + URL, body);
-  }
-
-  put<T>(URL : string, body:any): Observable<HttpEvent<T>> {
-    return this.http.get<T>(environment.apiBaseUrl + URL, body);
-  }
-
-  delete<T>(URL : string): Observable<T> {
-    return this.http.get<T>(environment.apiBaseUrl + URL);
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Une erreur s\'est produite:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
