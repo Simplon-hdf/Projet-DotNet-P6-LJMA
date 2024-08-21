3adf6cbd3f9810d61076d1abdd74331b1d3e78ba
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-21 12:15:21 +0200

    feat(sejours): Création du model/composant/services pour gerer les randonnées

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/sejour/sejour.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/sejour/sejour.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
-import {HeaderComponent} from "../header/header.component";
+import {Component} from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FormsModule } from "@angular/forms";
+import {RandonneeService} from "../../Services/randonnee.service";
+import {Randonnee} from "../../Models/randonnee";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-sejour',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    FormsModule,
+    NgForOf,
   ],
   templateUrl: './sejour.component.html',
   styleUrl: './sejour.component.css'
 })
-export class SejourComponent {
 
+export class SejourComponent {
+  title = 'Randonnées';
+  randos: Randonnee[] = [];
+
+  constructor(private randonneeService: RandonneeService) {}
+
+  ngOnInit(): void {
+    this.getRandos();
+  }
+
+  getRandos(): void {
+    this.randonneeService.getRandos().subscribe(
+      (result: Randonnee[]) => {
+        this.randos = result;
+      },
+      (error) => {
+        console.error('Error fetching randos:', error);
+      }
+    );
+  }
 }
