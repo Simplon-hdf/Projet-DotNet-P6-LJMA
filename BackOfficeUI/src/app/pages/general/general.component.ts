323daff73276905904dda4f598e5c036bc22566e
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-22 11:33:26 +0200

    refactor(): add imports

--- BackOfficeUI/src/app/pages/general/general.component.ts
+++ BackOfficeUI/src/app/pages/general/general.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../shared/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-general',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './general.component.html',
   styleUrl: './general.component.css'
 })
