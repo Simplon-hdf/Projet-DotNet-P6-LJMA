ba6abdbc7ef8010d8f6acd0713151788f5162678
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-22 11:40:53 +0200

    feat(app.routes.ts): Ajout de la route pour la page about

--- Projet-DotNet-P6-LJMA-Front/src/app/app.routes.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from "./Components/register/register.component";
 import {HomeComponent} from "./Pages/home/home.component";
 import {BurgerMenuComponent} from "./Components/burger-menu/burger-menu.component";
 import {SejourComponent} from "./Components/sejour/sejour.component";
+import {AboutComponent} from "./Pages/about/about.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: BurgerMenuComponent },
   { path: 'sejour', component: SejourComponent },
+  { path: 'about', component: AboutComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
