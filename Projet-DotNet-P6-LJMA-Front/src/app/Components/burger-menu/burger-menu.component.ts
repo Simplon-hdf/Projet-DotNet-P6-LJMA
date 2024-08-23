8ad500c67487214416abbd44d5b04a3d8539a789
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-23 16:39:53 +0200

    fix(login/register): Ajout des fonction de connexion et register....

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/burger-menu/burger-menu.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/burger-menu/burger-menu.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {LoginButtonComponent} from "../login-button/login-button.component";
 import {RegisterButtonComponent} from "../register-button/register-button.component";
+import { LoginService } from '../../Services/login.service';
+import { LogoutButtonComponent } from "../logout-button/logout-button.component";
 
 @Component({
   selector: 'app-burger-menu',
@@ -9,11 +11,12 @@ import {RegisterButtonComponent} from "../register-button/register-button.compon
   imports: [
     RouterLink,
     LoginButtonComponent,
-    RegisterButtonComponent
-  ],
+    RegisterButtonComponent,
+    LogoutButtonComponent
+],
   templateUrl: './burger-menu.component.html',
   styleUrl: './burger-menu.component.css'
 })
 export class BurgerMenuComponent {
-
+  loginService = inject(LoginService);
 }
