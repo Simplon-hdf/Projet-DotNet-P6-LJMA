23b926f72f426ea9fa8eb9d2df38594580b6e734
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-23 16:39:33 +0200

    feat(login): Ajout des fonction login

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/login-button/login-button.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/login-button/login-button.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, Injectable, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import { LoginService } from '../../Services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-button',
@@ -10,6 +12,12 @@ import {RouterLink} from "@angular/router";
   templateUrl: './login-button.component.html',
   styleUrl: './login-button.component.css'
 })
-export class LoginButtonComponent {
 
+export class LoginButtonComponent {
+  loginService = inject(LoginService);
+  router = inject(Router);
+
+  login(){
+    this.router.navigate(['login']);
+  }
 }
