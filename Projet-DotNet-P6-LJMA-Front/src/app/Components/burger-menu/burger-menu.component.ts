3daf7226ef8145509ace54eaca9c53842e82bf6f
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-23 09:30:00 +0200

    feat(burger-menu): Ajout des mediasquery

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/burger-menu/burger-menu.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/burger-menu/burger-menu.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {LoginButtonComponent} from "../login-button/login-button.component";
+import {RegisterButtonComponent} from "../register-button/register-button.component";
 
 @Component({
   selector: 'app-burger-menu',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    LoginButtonComponent,
+    RegisterButtonComponent
   ],
   templateUrl: './burger-menu.component.html',
   styleUrl: './burger-menu.component.css'
