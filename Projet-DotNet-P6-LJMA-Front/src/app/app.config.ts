e20f29ee108ee3655484da2b17e8a73b920ee8fd
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-23 14:22:14 +0200

    fix(route, config)

--- Projet-DotNet-P6-LJMA-Front/src/app/app.config.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withFetch} from "@angular/common/http";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())]
+  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideAnimationsAsync()]
 };
--- Projet-DotNet-P6-LJMA-Front/src/app/app.routes.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/app.routes.ts
@@ -4,15 +4,15 @@ import {LoginComponent} from "./Components/login/login.component";
 import { RegisterComponent } from "./Components/register/register.component";
 import {HomeComponent} from "./Pages/home/home.component";
 import {BurgerMenuComponent} from "./Components/burger-menu/burger-menu.component";
-import {SejourComponent} from "./Components/sejour/sejour.component";
 import {AboutComponent} from "./Pages/about/about.component";
+import {RandonneeDetailsComponent} from "./Pages/randonnee-details/randonnee-details.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signin', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: BurgerMenuComponent },
-  { path: 'sejour', component: SejourComponent },
+
   { path: 'about', component: AboutComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
