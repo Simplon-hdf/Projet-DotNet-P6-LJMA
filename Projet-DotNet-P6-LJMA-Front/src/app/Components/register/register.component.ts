0fd18dea57365b1d0cc38b16ed9537591e690043
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-23 14:20:56 +0200

    feat(register): Ajout register

--- Projet-DotNet-P6-LJMA-Front/src/app/Components/register/register.component.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/Components/register/register.component.ts
@@ -1,45 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from "@angular/router";
-import { FormsModule, NgForm } from "@angular/forms";
-import { FormRegisterService } from "../../Services/form-register.service";
+import {Component, inject, OnDestroy} from '@angular/core';
+import { Router } from "@angular/router";
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import { UserRegister } from "../../Models/reg.model";
+import {MatButton} from "@angular/material/button";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {RegisterService} from "../../Services/register.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    RouterLink,
-    FormsModule,
-    RouterOutlet,
-  ],
+    imports: [
+        FormsModule,
+        MatButton,
+        MatError,
+        MatFormField,
+        MatInput,
+        MatLabel,
+        ReactiveFormsModule,
+    ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent implements OnInit {
-  user: UserRegister = {};
-  constructor(public service : FormRegisterService, public router: Router ) {}
+export class RegisterComponent implements OnDestroy {
+  private formBuilder = inject(FormBuilder);
+  private registerService = inject(RegisterService);
+  private registerSubscription: Subscription | null = null;
+  private router = inject(Router);
 
-  onSubmit(form: NgForm){
-    this.service.postEnregistrer()
-      .subscribe({
-        next: res => {
-          console.log(res)
-          alert('Utilisateur crée');
-          this.router.navigate(['/home']);
-        },
-        error: err =>{console.log(err), alert('Utilisateur non crée');}
-      })
+  registerFormGroup = this.formBuilder.group({
+    nom: ['', Validators.required],
+    prenom: ['', Validators.required],
+    telephone: ['', Validators.required],
+    email: ['', Validators.required],
+    motdepasse: ['', Validators.required],
+  });
+
+  register() {
+    if (this.registerFormGroup.valid) {
+      const userRegister: UserRegister = this.registerFormGroup.value as UserRegister;
+      this.registerSubscription = this.registerService
+        .register(userRegister)
+        .subscribe({
+          next: (result) => {
+            console.table(result);
+            if (result) {
+              this.router.navigate(['home']);
+            } else {
+              console.log('Registration failed: No result returned');
+            }
+          },
+          error: (error) => {
+            console.error('Registration error:', error);
+            // Optionally display an error message to the user
+          },
+        });
+    } else {
+      console.log('Form is invalid');
+      Object.values(this.registerFormGroup.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
   }
 
-  isValidLogin(): boolean{
-    const valideurUtilisateur : RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    return valideurUtilisateur.test(this.user.email!);
+  ngOnDestroy() {
+    this.registerSubscription?.unsubscribe();
   }
-
-  isValidPassword(): boolean{
-    const valideurMotDePasse  : RegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[\w!@#$%^&*(),.?":{}|<>]{12,}$/gm;
-    return valideurMotDePasse.test(this.user.motDePasse!);
-  }
-
-  ngOnInit() {}
 }
