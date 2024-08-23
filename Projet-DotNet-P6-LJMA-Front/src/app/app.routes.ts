f50a8aee438b4958d529b122782309576b170451
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-23 16:41:09 +0200

    fix(routes): resolution probleme route

--- Projet-DotNet-P6-LJMA-Front/src/app/app.routes.ts
+++ Projet-DotNet-P6-LJMA-Front/src/app/app.routes.ts
@@ -5,14 +5,12 @@ import { RegisterComponent } from "./Components/register/register.component";
 import {HomeComponent} from "./Pages/home/home.component";
 import {BurgerMenuComponent} from "./Components/burger-menu/burger-menu.component";
 import {AboutComponent} from "./Pages/about/about.component";
-import {RandonneeDetailsComponent} from "./Pages/randonnee-details/randonnee-details.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signin', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: BurgerMenuComponent },
-
   { path: 'about', component: AboutComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
